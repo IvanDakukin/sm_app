f997d8dbfc48af7089ea0fc43647a21372848535
Ivan Dakukin <ivdakukin@gmail.com>
2024-03-21 01:14:34 +0300

    Добавлен поле автора статьи

--- src/components/ArticleNode.vue
+++ src/components/ArticleNode.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="article" v-bind:class="{ article_published: isPublished }">
-    <div class="article__title">
+    <div class="article__head">
       <h2>{{ title }}</h2>
+      <div class="article__author">
+        {{ authorName }}
+      </div>
     </div>
     <div class="article__body">
       <p>{{ body }}</p>
@@ -16,19 +19,26 @@ export default {
     title: String,
     body: String,
     isPublished: Boolean,
+    author: String,
+  },
+  computed: {
+    authorName() {
+      return this.isPublished ? this.author.toUpperCase() : this.author;
+    },
   },
 };
 </script>
 
 <style scoped>
 .article {
+  border-radius: 10px;
   max-height: 350px;
   width: 320px;
   -webkit-box-shadow: 0px 5px 10px 2px rgba(34, 60, 80, 0.2);
   -moz-box-shadow: 0px 5px 10px 2px rgba(34, 60, 80, 0.2);
   box-shadow: 0px 5px 10px 2px rgba(34, 60, 80, 0.2);
 }
-.article__title {
+.article__head {
   text-align: center;
   border-bottom: 1px solid rgba(0, 0, 0, 0.494);
 }
@@ -36,6 +46,10 @@ export default {
   padding: 10px;
 }
 .article_published {
-  background-color: rgb(255, 126, 126);
+  background-color: #ff929d;
+}
+.article__author {
+  color: rgba(54, 54, 54, 0.715);
+  padding-bottom: 10px;
 }
 </style>
