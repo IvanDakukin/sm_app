f26bd597b0baa154d55a14715151a4d54fd233aa
Ivan Dakukin <ivdakukin@gmail.com>
2024-03-28 01:37:53 +0300

    Добавлен роутер

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import ArticleList from "@/views/ArticleList.vue";
+import AboutView from "@/views/AboutView.vue";
+import ArticleNode from "@/components/ArticleNode.vue";
+import ArticleForm from "@/components/ArticleForm.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "articles",
+    component: ArticleList,
+  },
+  {
+    path: "/about",
+    name: "О нас",
+    component: AboutView,
+  },
+  {
+    path: "/article/:id",
+    component: ArticleNode,
+  },
+  {
+    path: "/new",
+    name: "Добавить статью",
+    component: ArticleForm,
+  },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+export default router;
